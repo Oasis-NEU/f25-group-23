f2270393e40723d7617fd6732fb5aa3a6b756643
Pranathi Arun Senthil <pranathiarun7@gmail.com>
2025-10-19 13:51:30 -0400

    slay

---
+++ src/grocery-ecommerce/app/page.js
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+  <div>
+  <h2>Oasis Project, Group 2: Pranathi, Saloni, Shreeya, Cassie, Ananya</h2>
+  <Button>Pick your groceries</Button>
+  </div>
+  );
+}

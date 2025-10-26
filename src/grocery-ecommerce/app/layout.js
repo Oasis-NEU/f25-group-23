9f00c13621bd3cdc4259171fb281605f93bebff6
Pranathi Arun Senthil <pranathiarun7@gmail.com>
2025-10-26 14:27:58 -0400

    worked on home

--- src/grocery-ecommerce/app/layout.js
+++ src/grocery-ecommerce/app/layout.js
@@ -1,14 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Outfit} from "next/font/google";
 import "./globals.css";
+import Header from "./_components/Header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const outfit = Outfit({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: "--font-outfit",
 });
 
 export const metadata = {
@@ -20,8 +16,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        className={`${outfit.variable} antialiased`}
+      > 
+      <Header/>
         {children}
       </body>
     </html>
--- src/grocery-ecommerce/app/page.js
+++ src/grocery-ecommerce/app/page.js
@@ -4,8 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
   <div>
-  <h2>Oasis Project, Group 2: Pranathi, Saloni, Shreeya, Cassie, Ananya</h2>
-  <Button>Pick your groceries</Button>
+    Home
   </div>
   );
 }

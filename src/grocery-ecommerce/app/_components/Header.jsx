16183905dc9814a52b11c77188cf17ecf98f004c
Pranathi Arun Senthil <pranathiarun7@gmail.com>
2025-11-02 12:57:15 -0500

    slay

--- src/grocery-ecommerce/app/_components/Header.jsx
+++ src/grocery-ecommerce/app/_components/Header.jsx
@@ -4,6 +4,15 @@ import { ShoppingBag } from 'lucide-react'
 import Image from 'next/image'
 import React from 'react'
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
 function Header() {
   return (
     <div className='p-5 shadow-md flex justify-between'>
@@ -12,11 +21,26 @@ function Header() {
         width={150}
         height={100}
         />
-        
-        <h2 className='flex gap-2 items-center
+          <DropdownMenu>
+          <DropdownMenuTrigger>
+           <h2 className='flex gap-2 items-center
             border rounded-full p-2 px-10 bg-slate-200
-        '>
+        '> 
           <LayoutGrid className='h-5 w-5' />Category</h2>
+          </DropdownMenuTrigger>
+          <DropdownMenuTrigger>
+
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem>
+          </DropdownMenuContent>
+          </DropdownMenu>
+
         <div className=' md:flex gap-3 border rounded-full p-2 px-5
         hidden'>
           <Search/>
@@ -25,6 +49,7 @@ function Header() {
           className='outline-none'
           />
         </div>
+
     </div>
     <div className='flex gap-5 items-center'>
       <h2 className='flex gap-2 items-center text-lg'> <ShoppingBag/> 0</h2>
